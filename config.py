a89ba97d05d048ccee9ac0476118de2dd8314c61
Noha9900 <150599591+Noha9900@users.noreply.github.com>
2026-02-20 23:29:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# config.py
+
+BOTS = [
+    {
+        "BOT_TOKEN": "BOT_TOKEN_1",
+        "MONGO_URI": "mongodb+srv://user:pass@cluster/db1",
+        "ADMIN_IDS": [123456789],
+        "CHANNEL_ID": None
+    },
+    {
+        "BOT_TOKEN": "BOT_TOKEN_2",
+        "MONGO_URI": "mongodb+srv://user:pass@cluster/db2",
+        "ADMIN_IDS": [987654321],
+        "CHANNEL_ID": -100xxxxxxxxxx
+    }
+]
+
+API_ID = 123456
+API_HASH = "YOUR_API_HASH"
+
+DOWNLOAD_PATH = "./downloads"
+MAX_DOWNLOAD = 2
+MAX_UPLOAD = 1
